02ae83230456c1326601586ad94c03ba00c07cdd
us͡an̸df͘rien͜ds͠ <usandfriends@gmail.com>
2016-09-10 18:56:54 -0700

    Initial routes commit

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ app.engine('html', require('ejs').renderFile);
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/css', express.static(__dirname + '/public/css'));
 
-app.get('/', function(req, res) {
-	res.render('index.html');
-});
+app.use('/', require('./lib/routes/index'));
 
 app.listen(3000, function() {
 	console.log('Listening on port 3000.');
